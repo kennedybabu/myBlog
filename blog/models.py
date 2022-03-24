3cc928142a8fb4c419aa2caade9834eed8a54fe2
kennedybabu <kennedynjogu8@gmail.com>
2022-03-24 20:46:24 +0300

    fix: fix issue with canonical URLs

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
-class PublishedManager(models.Model):
+class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager,
                      self).get_queryset()\
@@ -26,6 +27,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                        self.publish.month,
+                        self.publish.day, self.slug])
 
     def __str__(self):
         return self.title
