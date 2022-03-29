94d965b2fd091e6ec559abbf02aadd75e8c9b8c8
kennedybabu <kennedynjogu8@gmail.com>
2022-03-29 10:10:23 +0300

    feature: use a class based view for the PostListView view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3) # 3 posts in each page
@@ -10,7 +18,7 @@ def post_list(request):
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        # if pae is not an int deliver the first page
+        # if page is not an int deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
         # if page is out of range deliver last page of results
